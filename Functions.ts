53d1a97a904c345131431ddaff685dee3f29e08e
samir <ahmed.elsaka@bosta.co>
2023-08-14 15:51:08 +0300

    section(Function): add function.ts contains concepts around function

---
+++ Functions.ts
@@ -0,0 +1,10 @@
+/**
+ * Functions are simply transformation. it reveives an imput and transform them to somthing else.
+ * Inputs and Outputs are sets of values.
+ * Functions can only map a value from input ton only one value to the output universe.
+ * Function is Deterministic if : Every time calling a function we're expecting to return the same value.
+ * Functions should not have side effects.
+ * Pure function: has no side effects, deterministic and total.
+ * Varaiables are immutable, we only create new values from old ones.
+ * Referential Transparency: you can replace a function with its definition
+ */
